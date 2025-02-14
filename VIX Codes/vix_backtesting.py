c022197cdaa18306d29b2e2a94b6406056259678
Andrew Mapes <andrew.c.mapes@gmail.com>
2025-02-14 09:33:52 -0600

    "Updating file paths"
    Please enter the commit message for your changes. Lines starting

--- VIX Codes/vix_backtesting.py
+++ VIX Codes/vix_backtesting.py
@@ -14,17 +14,17 @@ def dollar_format(x, _):
 #
 #
 
-def Load():
+def load():
     """
-    Load function pulls both the differential data and the spread data for analysis
+    load function pulls both the differential data and the spread data for analysis
 
     """
-    file_spreads1 = r"C:\Users\andre\spyder-env\Projects\VIX\VIX Processed Data\1 Month Spreads.csv"
-    file_spreads2 = r"C:\Users\andre\spyder-env\Projects\VIX\VIX Processed Data\2 Month Spreads.csv"
-    file_spreads3 = r"C:\Users\andre\spyder-env\Projects\VIX\VIX Processed Data\3 Month Spreads.csv"
-    file_diff1 = r"C:\Users\andre\spyder-env\Projects\VIX\VIX Processed Data\1 Month Differentials.csv"
-    file_diff2 = r"C:\Users\andre\spyder-env\Projects\VIX\VIX Processed Data\2 Month Differentials.csv"
-    file_diff3 = r"C:\Users\andre\spyder-env\Projects\VIX\VIX Processed Data\3 Month Differentials.csv"
+    file_spreads1 = r"c:\Users\andre\VS Code\Projects\VIX\VIX Processed Data\1 month Spreads.csv"
+    file_spreads2 = r"c:\Users\andre\VS Code\Projects\VIX\VIX Processed Data\2 month Spreads.csv"
+    file_spreads3 = r"c:\Users\andre\VS Code\Projects\VIX\VIX Processed Data\3 month Spreads.csv"
+    file_diff1 = r"c:\Users\andre\VS Code\Projects\VIX\VIX Processed Data\1 month Differentials.csv"
+    file_diff2 = r"c:\Users\andre\VS Code\Projects\VIX\VIX Processed Data\2 month Differentials.csv"
+    file_diff3 = r"c:\Users\andre\VS Code\Projects\VIX\VIX Processed Data\3 month Differentials.csv"
     data_spreads = []
     data_spreads.append(pd.read_csv(file_spreads1))
     data_spreads.append(pd.read_csv(file_spreads2))
@@ -36,9 +36,9 @@ def Load():
     return data_spreads, data_diffs
 
 
-def Strategy(data, buy_cutoff=.3, buy_month=5, spread=.75, quantity=8):
+def strategy(data, buy_cutoff=.3, buy_month=5, spread=.75, quantity=8):
     """
-    Strategy backtests different buying and selling strategies for future spreads. The data used
+    strategy backtests different buying and selling strategies for future spreads. The data used
     goes back to August 2013 as seen from the merged data from which spreads are generated. It works
     by identifying criteria for purchase, criteria for sale, and calculates all proceeds from the 
     transactions after subtracting the trading fees.
@@ -64,7 +64,7 @@ def Strategy(data, buy_cutoff=.3, buy_month=5, spread=.75, quantity=8):
         DESCRIPTION. losses gives the list of all payoffs from non-profitable trades. 
 
     """
-    fees = 9.52 # Cost of buying and selling a single future spread
+    fees = 9.52 # cost of buying and selling a single future spread
     multiplier = 1000 # Value multiplier from spread price
     gains = []
     losses = []
@@ -83,9 +83,9 @@ def Strategy(data, buy_cutoff=.3, buy_month=5, spread=.75, quantity=8):
     return gains, losses
 
 
-def Spread_Comparison(data, buy_cutoff=.3, buy_month=5, spreads: list=[.75]):
+def spread_comparison(data, buy_cutoff=.3, buy_month=5, spreads: list=[.75]):
     """
-    Spread_Comparison is designed to aid in evaluating the effectiveness of different sell points
+    spread_comparison is designed to aid in evaluating the effectiveness of different sell points
     determined by the buy price and the spread. The spread value determines how much the value needs
     to increase from the buy point for the contract to be sold early. 
 
@@ -115,16 +115,16 @@ def Spread_Comparison(data, buy_cutoff=.3, buy_month=5, spreads: list=[.75]):
     losses = []
     profits = []
     for spread in spreads:
-        gain, loss = Strategy(data = data, buy_month=buy_month, spread=spread, buy_cutoff=buy_cutoff)
+        gain, loss = strategy(data = data, buy_month=buy_month, spread=spread, buy_cutoff=buy_cutoff)
         gains.append(sum(gain))
         losses.append(sum(loss))
         profits.append(sum(gain+loss))
     return profits, gains, losses
 
 
-def Buy_Cutoff_Comparison(data, buy_cutoffs: list=[.3], buy_month=5, spread=.75):
+def buy_cutoff_comparison(data, buy_cutoffs: list=[.3], buy_month=5, spread=.75):
     """
-    The Buy_Cutoff_Comparison function is used to compare different buy point cutoffs to identify efficient
+    The buy_cutoff_comparison function is used to compare different buy point cutoffs to identify efficient
     values in comparing profit vs exposure.
 
     Parameters
@@ -153,16 +153,16 @@ def Buy_Cutoff_Comparison(data, buy_cutoffs: list=[.3], buy_month=5, spread=.75)
     losses = []
     profits = []
     for buy_cutoff in buy_cutoffs:
-        gain, loss = Strategy(data = data, buy_cutoff = buy_cutoff, buy_month = buy_month, spread = spread)
+        gain, loss = strategy(data = data, buy_cutoff = buy_cutoff, buy_month = buy_month, spread = spread)
         gains.append(sum(gain))
         losses.append(sum(loss))
         profits.append(sum(gain+loss))
     return profits, gains, losses
 
 
-def Buy_Month_Comparison(data, buy_cutoff=.3, buy_months: list=[5], spread=.75):
+def buy_month_comparison(data, buy_cutoff=.3, buy_months: list=[5], spread=.75):
     """
-    The Buy_Month_Comparison function is used to compare different purchase periods to identify efficient
+    The buy_month_comparison function is used to compare different purchase periods to identify efficient
     practices in comparing profit vs exposure.
 
     Parameters
@@ -191,16 +191,16 @@ def Buy_Month_Comparison(data, buy_cutoff=.3, buy_months: list=[5], spread=.75):
     losses = []
     profits = []
     for buy_month in buy_months:
-        gain, loss = Strategy(data = data, buy_cutoff = buy_cutoff, buy_month = buy_month, spread = spread)
+        gain, loss = strategy(data = data, buy_cutoff = buy_cutoff, buy_month = buy_month, spread = spread)
         gains.append(sum(gain))
         losses.append(sum(loss))
         profits.append(sum(gain+loss))
     return profits, gains, losses
 
 
-def Comparison_Grapher(data, entries: list, comparable: str='spreads', spread=.75, buy_cutoff=.3, buy_month=5) -> None:
+def comparison_grapher(data, entries: list, comparable: str='spreads', spread=.75, buy_cutoff=.3, buy_month=5) -> None:
     """
-    The Comparison_Grapher function is used to produce graphs of the comparisons of buy_points, spreads, or purchase months.
+    The comparison_grapher function is used to produce graphs of the comparisons of buy_points, spreads, or purchase months.
     It calls on the specific functions for comparison defined above. Then using the results produces plots to better visualize
     this information. 
 
@@ -231,14 +231,14 @@ def Comparison_Grapher(data, entries: list, comparable: str='spreads', spread=.7
     ax1.yaxis.set_major_formatter(ff(dollar_format)) # provides formatting for the 2 y axes
     ax2.yaxis.set_major_formatter(ff(dollar_format))
     if comparable == 'spreads':
-        p, g, l = Spread_Comparison(data=data, spreads=entries, buy_cutoff=buy_cutoff ,buy_month=buy_month)
-        plt.title('Spreads Comparison')
+        p, g, l = spread_comparison(data=data, spreads=entries, buy_cutoff=buy_cutoff ,buy_month=buy_month)
+        plt.title('Spreads comparison')
     elif comparable == 'months':
-        p, g, l = Buy_Month_Comparison(data=data, buy_months=entries, buy_cutoff=buy_cutoff, spread=spread)
-        plt.title('Months Comparison')
+        p, g, l = buy_month_comparison(data=data, buy_months=entries, buy_cutoff=buy_cutoff, spread=spread)
+        plt.title('months comparison')
     elif comparable == 'buys':
-        p, g, l = Buy_Cutoff_Comparison(data=data, buy_cutoffs=entries, buy_month=buy_month, spread=spread)
-        plt.title('Buy Cutoff Comparison')
+        p, g, l = buy_cutoff_comparison(data=data, buy_cutoffs=entries, buy_month=buy_month, spread=spread)
+        plt.title('buy cutoff comparison')
     
     
     ax1.plot(entries, p, color='green', label='Profits')
@@ -249,19 +249,24 @@ def Comparison_Grapher(data, entries: list, comparable: str='spreads', spread=.7
     plt.show()
 
 
+def variable_sale_price(data, percent_price = 80, buy_cutoff=.3, buy_month=5, quantity=8):
+    pass
+
+
+
 # # # # #
 # Sample analysis codes
 #
 #
 #
 
-spread_data, diff_data = Load()
+spread_data, diff_data = load()
 
 buy_months = [i for i in range(2, 6)]
-Comparison_Grapher(data=spread_data[0], entries=buy_months, comparable='months')
+comparison_grapher(data=spread_data[0], entries=buy_months, comparable='months')
 
 spreads = [i/100 for i in range(110,250)]
-Comparison_Grapher(data=spread_data[0], entries=spreads, comparable='spreads')
+comparison_grapher(data=spread_data[0], entries=spreads, comparable='spreads')
 
 buy_cutoffs = [i/100 for i in range(60)]
-Comparison_Grapher(data=spread_data[0], entries=buy_cutoffs, comparable='buys')
+comparison_grapher(data=spread_data[0], entries=buy_cutoffs, comparable='buys')
